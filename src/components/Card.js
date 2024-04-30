42c3a42c70e1602823137e9888315cea5fbf0471
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2024-04-30 12:04:46 -0400

    new arch for assembly guides

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,9 +4,10 @@ import styles from './Card.module.css'; // Import the corresponding CSS
 import Link from '@docusaurus/Link'; // Import Link component for internal navigation
 
 
-const Card = ({ imageUrl, headerText, linkUrl }) => (
+const Card = ({ imageUrl, headerText, linkUrl, isNew }) => (
   <Link to={linkUrl} className={styles.cardLink}>
     <div className={styles.card}>
+      {isNew && <div className={styles.newLabel}>New</div>}
       <img src={require(`/static/img/${imageUrl}`).default} alt={headerText} className={styles.cardImage} />
       <h3 className={styles.cardHeader}>{headerText}</h3>
     </div>
