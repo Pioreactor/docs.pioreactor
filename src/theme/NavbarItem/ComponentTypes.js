2bd2d6cdbc0fedd0a95af14b85a9b343906d9a75
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2026-05-07 20:55:33 -0400

    Register latest version navbar item

---
+++ src/theme/NavbarItem/ComponentTypes.js
@@ -0,0 +1,7 @@
+import ComponentTypes from '@theme-original/NavbarItem/ComponentTypes';
+import LatestVersionNavbarItem from '@site/src/components/LatestVersionNavbarItem';
+
+export default {
+  ...ComponentTypes,
+  'custom-latestVersion': LatestVersionNavbarItem,
+};
